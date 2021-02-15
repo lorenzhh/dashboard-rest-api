c7870f57b3aeac58d9bba921052757a3609b050b
Lawrence Nseir <lawrence.nseir@gmail.com>
2021-02-15 09:01:23 +0100

    refactor: update mock data

--- mock-server/index.js
+++ mock-server/index.js
@@ -1,30 +1,34 @@
-const jsonServer = require('json-server');
-const catalogues = require('../catalogues/example.json');
-const users = require('../users/example.json');
+const jsonServer = require("json-server");
+const catalogues = require("../catalogues/example.json");
+const users = require("../users/example.json");
 
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 
 const rewriter = jsonServer.rewriter({
-    '/api/*': '/$1'
+  "/api/*": "/$1"
 });
 
 const router = jsonServer.router({
-    Catalogues: catalogues,
-    Users: users
+  Catalogues: catalogues,
+  Users: users
 });
 
 router.render = (request, response) => {
-    response.jsonp(
-        response.locals.data
-    )
+  if (response.locals.data.length === 0) {
+    response.status(401);
+  }
+  if (response.locals.data.length === 1) {
+    response.locals.data = response.locals.data[0];
+  }
+  response.jsonp(response.locals.data);
 };
 
 const port = 3000;
 
 server.use(middlewares);
-server.use(rewriter)
+server.use(rewriter);
 server.use(router);
 server.listen(port, () => {
-    console.log(`Server running at port ${port}`);
+  console.log(`Server running at port ${port}`);
 });
